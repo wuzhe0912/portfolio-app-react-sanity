cc43ac6b64583504e5b55d9d51b074504d10fe01
Pitt Wu <kgb00128@gmail.com>
2021-03-26 00:39:00 +0800

    feat：add react router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Home from './views/Home';
+import Experience from './views/Experience';
+import Projects from './views/Projects';
+import NavBar from './components/NavBar';
+
 function App() {
-  return <div className='App'></div>;
+  return (
+    <BrowserRouter>
+      <NavBar></NavBar>
+      <Switch>
+        <Route component={Home} path='/' exact></Route>
+        <Route component={Experience} path='/experience' exact></Route>
+        <Route component={Projects} path='/projects' exact></Route>
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/views/Experience.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Experience = () => {
+  return <div>Experiences</div>;
+};
+
+export default Experience;
