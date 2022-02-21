e477098a4dc04ebaf0cfc775bae146081d6dc081
Pitt Wu <pitt.wu@hlwtech.com.tw>
2022-02-21 09:31:50 +0800

    chore: clean old version

--- src/client.js
+++
@@ -1,6 +0,0 @@
-import SanityClient from '@sanity/client';
-
-export default SanityClient({
-  projectId: '4u5hz68l',
-  dataset: 'production',
-});
--- src/views/Experience.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Experience = () => {
-  return <div>Experiences</div>;
-};
-
-export default Experience;
